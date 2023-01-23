471fa9302cab2be1a77d09bd406287026175c983
christianbrusa <christianbrusa@hotmail.com>
2023-01-23 09:34:12 -0300

    Agrego clase Billetera como receptor

--- dds-wallet.ts
+++ dds-wallet.ts
@@ -1,3 +1,11 @@
 interface Transferencia {
   ejecutar(): void;
 }
+
+class Billetera {
+  constructor(cbu:number, saldoActual:number){
+    this.cbu = cbu;
+    this.saldoActual = saldoActual;
+  }
+  
+}
