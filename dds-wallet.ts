048da73f25763d97235b7ff9a1d7477965888203
christianbrusa <christianbrusa@hotmail.com>
2023-02-19 02:50:13 -0300

    Agrego clase Programar como comando concreto

--- dds-wallet.ts
+++ dds-wallet.ts
@@ -37,6 +37,25 @@ class Enviar implements Transferencia {
   }
 }
 
+class Programar implements Transferencia {
+  constructor(remitente: Billetera, destinatario: Billetera, montoEnviar: number, fecha: string){
+    this.remitente = remitente;
+    this.destinatario = destinatario;
+    this.montoEnviar = montoEnviar;
+    this.fecha = fecha;
+  }
+  
+  ejecutar(){
+    if(this.montoEnviar > this.remitente.saldoActual){
+      throw new Error("No es posible realizar la transferencia debido a que el monto a enviar supera el saldo disponible");
+    }
+    else{
+      this.remitente.restarSaldo(this.montoEnviar);
+      this.destinatario.sumarSaldo(this.montoEnviar);
+    }
+  }
+}
+
 class Usuario {
   constructor(billetera: Billetera){
     this.billetera = billetera;
